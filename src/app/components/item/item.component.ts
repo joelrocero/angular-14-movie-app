7536f7da03546ba6d90b242a465e946a00a118b7
Joel.Rocero <joel.rocero@outlook.com>
2022-07-21 14:55:46 +0800

    For deplyment

---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,18 @@
+import { Movie } from './../../models/movie';
+import { Component, Input, OnInit } from '@angular/core';
+import { IMAGES_SIZES } from '../../constants/images-sizes';
+
+@Component({
+  selector: 'item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.scss']
+})
+export class MovieItemComponent implements OnInit {
+  @Input() itemData: Movie | null = null;
+
+  imagesSizes = IMAGES_SIZES;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MovieDto, Movie, MovieVideoDto, MovieImages, MovieCredits } from '../models/movie';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { GenresDto } from '../models/genre';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+  baseUrl: string = 'https://api.themoviedb.org/3';
+  apiKey: string = 'f06a68b0cedf38d180627719537c7e17';
+
+  constructor(private http: HttpClient) {}
+
+  getMovies(type: string = 'upcoming', count: number = 12) {
+    return this.http.get<MovieDto>(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`).pipe(
+      switchMap((res) => {
+        return of(res.results.slice(0, count));
+      })
+    );
+  }
+
+  getMovie(id: string) {
+    return this.http.get<Movie>(`${this.baseUrl}/movie/${id}?api_key=${this.apiKey}`);
+  }
+
+  getMovieVideo(id: string) {
+    return this.http.get<MovieVideoDto>(`${this.baseUrl}/movie/${id}/videos?api_key=${this.apiKey}`).pipe(
+      switchMap((res) => {
+        return of(res.results);
+      })
+    );
+  }
+
+  getMoviesGenres() {
+    return this.http.get<GenresDto>(`${this.baseUrl}/genre/movie/list?api_key=${this.apiKey}`).pipe(
+      switchMap((res) => {
+        return of(res.genres);
+      })
+    );
+  }
+
+  getMoviesByGenre(genreId: string, page: number) {
+    return this.http
+      .get<MovieDto>(`${this.baseUrl}/discover/movie?with_genres=${genreId}&page=${page}&api_key=${this.apiKey}`)
+      .pipe(
+        switchMap((res) => {
+          return of(res.results);
+        })
+      );
+  }
+  getMovieImages(id: string) {
+    return this.http.get<MovieImages>(`${this.baseUrl}/movie/${id}/images?api_key=${this.apiKey}`);
+  }
+
+  getMovieCredits(id: string) {
+    return this.http.get<MovieCredits>(`${this.baseUrl}/movie/${id}/credits?api_key=${this.apiKey}`);
+  }
+
+  searchMovies(page: number, searchValue?: string) {
+    const uri = searchValue ? '/search/movie' : '/movie/popular';
+
+    return this.http
+      .get<MovieDto>(`${this.baseUrl}${uri}?page=${page}&query=${searchValue}&api_key=${this.apiKey}`)
+      .pipe(
+        switchMap((res) => {
+          return of(res.results);
+        })
+      );
+  }
+}
